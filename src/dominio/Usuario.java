cc901afa832455b446417a58b5d386aefe4c1609
Fray León Osorio Rivera <frayosorio@gmail.com>
2024-08-28 19:18:58 -0500

    Se implementa el patron Builder para la entidad USUARIO

---
+++ src/dominio/Usuario.java
@@ -0,0 +1,79 @@
+package dominio;
+
+public class Usuario {
+
+    private final String nombres; // requeridos
+    private final String apellidos; // requeridos
+
+    private final int edad; // opcional
+    private final String telefono; // opcional
+    private final String direccion; // opcional
+
+    public Usuario(UsuarioBuilder builder) {
+        this.apellidos = builder.apellidos;
+        this.nombres = builder.nombres;
+        this.edad = edad;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public static class UsuarioBuilder {
+        private final String nombres;
+        private final String apellidos;
+
+        private int edad;
+        private String telefono;
+        private String direccion;
+
+        public UsuarioBuilder(String nombres, String apellidos) {
+            this.nombres = nombres;
+            this.apellidos = apellidos;
+        }
+
+        public UsuarioBuilder telefono(String telefono) {
+            this.telefono = telefono;
+            return this;
+        }
+
+        public UsuarioBuilder direccion(String direccion) {
+            this.direccion = direccion;
+            return this;
+        }
+
+        public UsuarioBuilder edad(int edad) {
+            this.edad = edad;
+            return this;
+        }
+
+        public Usuario build() {
+            Usuario usuario = new Usuario(this);
+            validarUsuario(usuario);
+            return usuario;
+        }
+
+        public void validarUsuario(Usuario usuario) {
+            // validación de la información
+        }
+
+    }
+}
